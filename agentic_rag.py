387554ea056e976d12f323eeaa73cabe70809dfe
ch-rashid-ali <rashidalim514@gmail.com>
2026-07-10 00:53:34 +0500

    Add rag

--- agentic_rag.py
+++ agentic_rag.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from sentence_transformers import SentenceTransformer
 from pinecone import Pinecone, ServerlessSpec
 from langchain_groq import ChatGroq
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import PyPDFLoader
