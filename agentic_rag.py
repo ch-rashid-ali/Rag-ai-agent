b167fa66f0febf8ff2c67c9ea4828a6077eed9d3
ch-rashid-ali <rashidalim514@gmail.com>
2026-07-10 10:58:51 +0500

    add set

--- agentic_rag.py
+++ agentic_rag.py
@@ -117,7 +117,7 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # Accept user input
-if user_query := st.chat_input("PDF ke baare me kuch bhi puchein..."):
+if user_query := st.chat_input("write your question..."):
     with st.chat_message("user"):
         st.markdown(user_query)
     st.session_state.messages.append({"role": "user", "content": user_query})
